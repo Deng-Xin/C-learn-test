4d6be6c6440b634386b0e04744805370edbc66e3
Deng-Xin <isdengxin@qq.com>
2021-03-04 21:07:53 +0800

    添加ubuntu下int8_t等定义的文件、修改完善sizeof.c文件

---
+++ test/int8&16&32&64.c
@@ -0,0 +1,24 @@
+/*************************************************************************************
+ * Filename     : int8&16&32&64.c
+ * Author       : XIN
+ * Description  : ubuntu下int8_t、int16_t、int32_t、int64_t的定义
+ * Date         : 2021-03-04 20:46:39
+ * LastEditTime : 2021-03-04 20:49:27
+ * Version      :
+ * History      :
+ *************************************************************************************/
+/* Signed.  */
+
+typedef signed char          int8_t;
+typedef short int            int16_t;
+typedef int                  int32_t;
+typedef long int             int64_t;
+typedef long long int        int64_t;
+
+
+/* Unsigned.  */
+typedef unsigned char             uint8_t;
+typedef unsigned short int        uint16_t;
+typedef unsigned int              uint32_t;
+typedef unsigned long int         uint64_t;
+typedef unsigned long long int    uint64_t;
--- test/sizeof.c
+++ test/sizeof.c
@@ -1,19 +1,42 @@
+/*************************************************************************************
+ * Filename     : sizeof.c
+ * Author       : XIN
+ * Description  : 测试常见整数类型所占字节数
+ * Date         : 2021-03-02 10:49:10
+ * LastEditTime : 2021-03-05 11:03:02
+ * Version      :
+ * History      :
+ *************************************************************************************/
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <stdint.h>
 
 int main()
 {
-    unsigned long int a = 1;
-    unsigned long b = 1;
-    uint32_t c = 1;
-    uint64_t d =1;
+    /* Signed */
+    signed char          a=1;
+    short int            b=1;
+    int                  c=1;
+    long int             d=1;
+    long long int        e=1;
 
-    printf("unsigned long int: %zd \n",sizeof(a));
-    printf("unsigned long: %zd \n",sizeof(b));
-    printf("uint32_t: %zd \n",sizeof(c));
-    printf("uint64_t: %zd \n",sizeof(d));
+    int8_t  f=1;
+    int16_t g=1;
+    int32_t h=1;
+    int64_t i=1;
+
+    /* 在printf中%d用于int或者比int小的整数类型。比int小的类型被转型成int。
+       %ld用于long类型，%lld用于long long类型。 */
+
+    printf("sizeof signed char   : %zd, %#x\n", sizeof(a), a);
+    printf("sizeof short int     : %zd, %#x\n", sizeof(b), b);
+    printf("sizeof int           : %zd, %#x\n", sizeof(c), c);
+    printf("sizeof long int      : %zd, %#lx\n", sizeof(d), d);
+    printf("sizeof long long int : %zd, %#llx\n", sizeof(e), e);
+
+    printf("sizeof int8_t  : %zd, %#x\n", sizeof(f), f);
+    printf("sizeof int16_t : %zd, %#x\n", sizeof(g), g);
+    printf("sizeof int32_t : %zd, %#x\n", sizeof(h), h);
+    printf("sizeof int64_t : %zd, %#lx\n", sizeof(i), i);
 
     return 0;
 }
